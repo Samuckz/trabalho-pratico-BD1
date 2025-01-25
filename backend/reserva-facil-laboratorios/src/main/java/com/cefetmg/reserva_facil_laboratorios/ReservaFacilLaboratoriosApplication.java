3832edf840b04755b0d7a8a8a2ca12ff87418439
Samuel Santana <contato.santanasamuel@gmail.com>
2025-01-25 07:36:51 -0300

    first commit

---
+++ backend/reserva-facil-laboratorios/src/main/java/com/cefetmg/reserva_facil_laboratorios/ReservaFacilLaboratoriosApplication.java
@@ -0,0 +1,13 @@
+package com.cefetmg.reserva_facil_laboratorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaFacilLaboratoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaFacilLaboratoriosApplication.class, args);
+	}
+
+}
---
+++ backend/reserva-facil-laboratorios/src/test/java/com/cefetmg/reserva_facil_laboratorios/ReservaFacilLaboratoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefetmg.reserva_facil_laboratorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaFacilLaboratoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
